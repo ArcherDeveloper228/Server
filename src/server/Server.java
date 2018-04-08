1de25f4a6f9ec737f5b50b6af093b2c51207bbd8
Nikita Ustyushenko <nikita1999760@gmail.com>
2018-04-08 21:38:36 +0300

    fourth commit

--- src/server/Server.java
+++ src/server/Server.java
@@ -10,13 +10,13 @@ public class Server extends Thread {
 
 	/** Property - database */
 	private Database database;
-	
+
 	/** Property - server_socket */
 	private ServerSocket server_socket;
-	
+
 	/** Property - socket */
 	private Socket socket;
-	
+
 	/** Property - count_clients */
 	private int count_clients;
 
@@ -27,7 +27,7 @@ public class Server extends Thread {
 
 		this.count_clients = 0;
 		this.database = new Database();
-		
+
 		// выполняем создание сервер сокета с портом 9999
 		try {
 			this.server_socket = new ServerSocket(9999);
@@ -41,49 +41,103 @@ public class Server extends Thread {
 	public void run() {
 
 		while (true) {
-			
+
 			try {
-				
+
 				// ожидаем подключения клиента
 				this.socket = this.server_socket.accept();
-				
+
 				// наращиваем количество подключенных клиентов к серверу
 				this.count_clients++;
 				new ClientThread(this.socket).start();
-				
-				
+
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
 
 	}
 
+	/**
+	 * This method return object ServerSocket
+	 * @return value of the ServerSocket
+	 * */
 	public ServerSocket getServerSocket() {
 
 		return this.server_socket;
 
 	}
-	
+
+	/**
+	 * This method set value of the property server_socket
+	 * @param server_socket value of the object ServerSocket
+	 * */
 	public void setServerSocket(ServerSocket server_socket) {
-		
+
 		this.server_socket = server_socket;
-		
+
 	}
-	
+
+	/**
+	 * This method return count of the clients in server
+	 * @return value of the count clients
+	 * */
 	public int getCountClients() {
-		
+
 		return this.count_clients;
-		
+
 	}
-	
+
+	/**
+	 * This method set value of the count clients
+	 * @param count_clients value of the count clients
+	 * */
 	public void setConutClients(int count_clients) {
-		
+
 		this.count_clients = count_clients;
-		
+
 	}
-	
-	public Socket getSocket() {}
+
+	/**
+	 * This method return value of the object Socket
+	 * @return value of the socket
+	 * */
+	public Socket getSocket() {
+
+		return this.socket;
+
+	}
+
+	/**
+	 * This method set value of the socket
+	 * @param socket value of the socket
+	 * */
+	public void setSocket(Socket socket) {
+
+		this.socket = socket;
+
+	}
+
+	/**
+	 * This method return object Database
+	 * @return object Database
+	 * */
+	public Database getDatabase() {
+
+		return this.database;
+
+	}
+
+	/**
+	 * This method set value of the object Database
+	 * @param database value of the object Database
+	 * */
+	public void setDatabase(Database database) {
+
+		this.database = database;
+
+	}
 
 }
