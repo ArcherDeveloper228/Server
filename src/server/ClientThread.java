827a326c83a93f163a4f197abd464acabd935a70
Nikita Ustyushenko <nikita1999760@gmail.com>
2018-04-30 23:00:07 +0300

    fifteenth commit

--- src/server/ClientThread.java
+++ src/server/ClientThread.java
@@ -12,7 +12,7 @@ public class ClientThread extends Thread {
 
 	/** Property - socket */
 	private Socket socket;
-	
+
 	/** Property - database */
 	private Database database;
 
@@ -48,60 +48,67 @@ public class ClientThread extends Thread {
 
 		System.out.println("Client is running...");
 		UserComand user_command = null;
-			
+
 		user_command = this.server_interface.readMessage();
-			
+
 		switch (user_command.getCommand()) {
 
 		case "Registration": this.registrationUser(user_command.getUser()); break;
 		case "Authorization": this.authorizationUser(user_command.getUser()); break;
-			
+
 		}
 
 	}
-	
+
 	/**
-	 * This method check user in database 
+	 * This method check user in database
 	 * @param user value of the object User
 	 * */
 	private final void authorizationUser(User user) {
-		
+
 		String attention = new String("Check your login (password)!");
 		ResultSet result_set = null;
-		
+
 		try {
-			
+
 			result_set = this.database.getResult("SELECT * FROM user WHERE login=\"" + user.getUserLogin() + "\"");
-			
+
 			if (!result_set.next()) this.server_interface.writeMessage(attention);
-			else if (result_set.getInt(9) == user.getUserPassword()) this.server_interface.writeMessage("Ok");
+			else if (result_set.getInt(9) == user.getUserPassword() &&
+					result_set.getString(8).hashCode() == user.getUserLogin().hashCode()) this.server_interface.writeMessage("Ok");
 			else this.server_interface.writeMessage(attention);
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	/**
 	 * This method add user into database
 	 * @param user value of the object User
 	 * */
 	private final void registrationUser(User user) {
-		
+
+		ResultSet result_set = null;
+
 		try {
-			
-			if (!this.database.getResult("SELECT * FROM USER WHERE login=\"" + user.getUserLogin() + "\"").next()) {
-				
+
+			result_set = this.database.getResult("SELECT * FROM USER WHERE login=\"" + user.getUserLogin() + "\"");
+
+			if (result_set.next() && result_set.getString(8).hashCode() == user.getUserLogin().hashCode())
+				this.server_interface.writeMessage("This login is already exists!");
+			else if (!result_set.next() || (result_set.next() && result_set.getString(8).hashCode() != user.getUserLogin().hashCode())) {
+
 				this.database.addUser(user);
 				this.server_interface.writeMessage("Ok");
-				
+
 			} else this.server_interface.writeMessage("This login is already exists!");
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	/**
@@ -143,25 +150,25 @@ public class ClientThread extends Thread {
 		this.server_interface = server_interface;
 
 	}
-	
+
 	/**
 	 * This method return value of the object Socket
 	 * @return value of the object Socket
 	 * */
 	public Socket getSocket() {
-		
+
 		return this.socket;
-		
+
 	}
-	
+
 	/**
-	 * This method set value of the object Socket 
+	 * This method set value of the object Socket
 	 * @param socket value o the object Socket
 	 * */
 	public void setSocket(Socket socket) {
-		
+
 		this.socket = socket;
-		
-	} 
+
+	}
 
 }
